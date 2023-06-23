3d8aa30f99cdfea25f2cc8f6b53b4329b3cd364f
Syed-Noor-Akbar-KS <syednoorakbar007@gmail.com>
2023-06-24 00:06:24 +0500

    Add funcation of active which give players to action

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
+init();
 
 const btn = document.querySelectorAll(".btn");
 
 let player1=[];
 let player2=[];
+let activeplayer=0;
 
-
-
-btn.forEach((e,i)=>{
-    e.addEventListener("click",()=>{
-        e.classList.toggle('red');
-        console.log(i);
+const active = ()=>{
+if(activeplayer == 0){
+    btn.forEach((e,i)=>{
+        e.addEventListener("click",()=>{
+            // e.classList.toggle('red');
+            e.value = "X";
+            // console.log(i);
+            e.setAttribute('disabled','')
+        })
     })
+    activeplayer++;
+    console.log(activeplayer)
+
+}
+else{
+    btn.forEach((e,i)=>{
+        e.addEventListener("click",()=>{
+            // e.classList.toggle('red');
+            e.value = "O";
+            // console.log(i);
+            e.setAttribute('disabled','')
+        })
+    })
+    activeplayer--;
+    console.log(activeplayer)
+}
+}
+
+// btn.addEventListener("click",active())
+btn.forEach((x)=>{
+    x.addEventListener("click",active);
 })
+
+
+
+active();
+function init(){
+        document.querySelector('.won1').innerHTML = 0;
+        document.querySelector('.won2').innerHTML = 0;
+        document.querySelector('.loss1').innerHTML = 0;
+        document.querySelector('.loss2').innerHTML = 0;
+        // console.log("run");
+}
 
